8acf219dd632644d6bd928508dc1328640d8b2a6
Utkk1010 <utkarshutk10@gmail.com>
2024-09-27 22:39:11 +0530

    addons(2)

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -22,6 +23,23 @@ router.get("/add-new" , (req,res)=>{
     res.render("addBlog" , {
         user: req.user,
     });
+});
+
+router.get("/:id" , async(req,res)=>{
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  return res.render("blog" , {
+    user: req.user,
+    blog,
+  });
+});
+
+router.post("/comment/:blogId" , async(req,res)=>{
+  const comment = await Comment.create({
+    content: res.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/" , upload.single("coverImage")  , async(req,res)=>{
